b6b08b8aaef8ea929e88b67356a34d91e0620774
ipk1 <32953048+ipk1@users.noreply.github.com>
2020-10-22 15:03:01 +0530

    Create ISD.sh
    this will download all the scope intigriti and split it into 3 files
    
    1) reconscope.txt will have all the wld cards, the *'s have been removed to aid in automating the list to your recon script, some of the *'s are at the TLD level ir *.paypal.* would be paypal.com it replaced in the script
    2)ipscope. all the ip addresses and CIDR's are stored here
    3) intigriti.txt will have all the complete urls

---
+++ ISD.sh
@@ -0,0 +1,19 @@
+ curl 'https://api.intigriti.com/core/researcher/program'   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer <<token>>'  --compressed | jq -r '.[] | select(.status==3)| .companyHandle' | tee cohandle.txt
+
+
+  curl 'https://api.intigriti.com/core/researcher/program'   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer <<token>>'  --compressed | jq -r '.[] | select(.status==3) | .handle' | tee handle.txt
+
+paste cohandle.txt handle.txt | while read if of
+do
+
+curl 'https://api.intigriti.com/core/researcher/program/'$of'/'$if''   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer <<token>>'  --compressed | jq -r '.domains[] | .content[] | .endpoint' >> intiscope.txt
+
+
+
+done
+cat intiscope.txt | grep "*" | sed  's/\(.\{2\}\)//' | sed  's/*/com/g' | sort -u >> reconscope.txt
+cat intiscope.txt | egrep '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' >> ipscope.txt
+cat intiscope.txt | grep -v -e '*' -e '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | httprobe | tee intigriti.txt
+~/axeauto/./groups$1.sh intigriti allinit
+~/axeauto/./kid$1.sh intigriti2kid.txt $( /root/recon/intigriti/gid.txt)
+echo" urls complete"
