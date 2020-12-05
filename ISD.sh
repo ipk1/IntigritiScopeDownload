818cd2d8dcf5c4160b9396fcfb962ec8bfc3e404
ipk1 <32953048+ipk1@users.noreply.github.com>
2020-12-05 16:58:23 +0530

    Update ISD.sh

--- ISD.sh
+++ ISD.sh
@@ -1,12 +1,12 @@
- curl 'https://api.intigriti.com/core/researcher/program'   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer <<token>>'  --compressed | jq -r '.[] | select(.status==3)| .companyHandle' | tee cohandle.txt
+ curl 'https://api.intigriti.com/core/researcher/program'   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer '$1''  --compressed | jq -r '.[] | select(.status==3)| .companyHandle' | tee cohandle.txt
 
 
-  curl 'https://api.intigriti.com/core/researcher/program'   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer <<token>>'  --compressed | jq -r '.[] | select(.status==3) | .handle' | tee handle.txt
+  curl 'https://api.intigriti.com/core/researcher/program'   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer '$1''  --compressed | jq -r '.[] | select(.status==3) | .handle' | tee handle.txt
 
 paste cohandle.txt handle.txt | while read if of
 do
 
-curl 'https://api.intigriti.com/core/researcher/program/'$of'/'$if''   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer <<token>>'  --compressed | jq -r '.domains[] | .content[] | .endpoint' >> intiscope.txt
+curl 'https://api.intigriti.com/core/researcher/program/'$of'/'$if''   -H 'authority: api.intigriti.com'   -H 'accept: application/json, text/plain, */*'   -H 'dnt: 1'   -H 'authorization: Bearer '$1''  --compressed | jq -r '.domains[] | .content[] | .endpoint' >> intiscope.txt
 
 
 
